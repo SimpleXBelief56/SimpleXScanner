f0980641513a5375bef2026fd4634f414a1e8832
SimpleXBelief56 <36784078+SimpleXBelief56@users.noreply.github.com>
2020-06-25 03:44:29 -0400

    Update scan.py
    Fix Scanning Issues

--- scan.py
+++ scan.py
@@ -3,25 +3,32 @@ import random
 import os
 import sys
 import nmap
+import platform
 from termcolor import colored
 
 class NetworkScanner():
 	def __init__(self):
 		self.deviceCounter = 0
 		self.network = nmap.PortScanner()
+		self.deviceInformation = []
 
-	def ScanNetwork(self):
-		self.hosts_dict = self.network.scan(hosts='192.168.1.1/24', arguments='-sP')
-		print "IP ADDRESS \t\t MAC ADDRESS \t\t HOST NAME"
+	def clear(self):
+		if platform.system() == "Windows":
+			os.system("cls")
+		else:
+			os.system("clear")
+
+	def ScanNetwork(self, networkDefaultGetaway):
+		self.hosts_dict = self.network.scan(hosts='{}/24'.format(networkDefaultGetaway), arguments='-sn')
+		self.clear()
+		print colored("IP ADDRESS \t\t MAC ADDRESS \t\t HOST NAME", "cyan", attrs=['bold'])
 		for scan_section in self.hosts_dict['scan']:
 			self.deviceCounter += 1
 			for host_information in self.hosts_dict['scan'][scan_section]:
 				if host_information == 'vendor':
 					for host_name in self.hosts_dict['scan'][scan_section][host_information]:
-						print colored("[{}]: {} \t {} \t {}".format(self.deviceCounter, scan_section, host_name, self.hosts_dict['scan'][scan_section][host_information][host_name]), "blue")
-						
-			time.sleep(0.1)
+						self.deviceInformation.append([str(scan_section), str(host_name)])
 
-		return self.deviceCounter
+		return self.deviceInformation
 
 
