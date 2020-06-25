db51d33fa4142487e67b75dd793726b9bfdfa667
SimpleXBelief56 <36784078+SimpleXBelief56@users.noreply.github.com>
2020-06-25 03:43:42 -0400

    Update SimpleXScanner.py
    Fix Scanning Issues

--- SimpleXScanner.py
+++ SimpleXScanner.py
@@ -2,12 +2,46 @@ import time
 import random
 import os
 import sys
+import requests
+import platform
 from scan import NetworkScanner
 from termcolor import colored
 
-os.system("clear")
+if platform.system() == "Windows":
+    print "ERROR: SimpleXScanner only supports Linux"
+    exit()
+else:
+    if os.geteuid() != 0:
+        print "ERROR: Run script as root with sudo"
+        exit()
+
+def clear():
+    if platform.system() == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+defaultNetworkGetaway = raw_input("Default Getaway: ")
+
+clear()
+print "Scanning Network, Please Wait....."
 nmap = NetworkScanner()
-NumberOfDevices = nmap.ScanNetwork()
+scanInformation = nmap.ScanNetwork(defaultNetworkGetaway)
+vendorNames = []
+
+url = "http://macvendors.co/api/"
+
+for counter in range(len(scanInformation)):
+    mac = scanInformation[counter][1]
+    mac_vendor_requests = requests.get(url+mac+"/json")
+    mac_vendor_json = mac_vendor_requests.json()
+    vendorNames.append(mac_vendor_json['result']['company'])
+
+
+for connectedDevicesCounter in range(len(scanInformation)):
+    print colored("[{}]: {} \t {} \t {}".format(connectedDevicesCounter, scanInformation[connectedDevicesCounter][0], scanInformation[connectedDevicesCounter][1], vendorNames[connectedDevicesCounter]), "blue")
+    time.sleep(0.1)
 print "------------------------"
 time.sleep(0.1)
 print colored("Connected Devices: {}".format(NumberOfDevices), "blue")
